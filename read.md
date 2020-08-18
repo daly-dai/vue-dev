91425f09ffd284bc5f0f770b0eb6b70047ec9faa
qianye <qianye@yscredit.com>
2020-08-18 10:58:28 +0800

    first commit

---
+++ read.md
@@ -0,0 +1,158 @@
+├─.circleci
+├─.github
+│  └─ISSUE_TEMPLATE
+├─benchmarks
+│  ├─big-table
+│  ├─dbmon
+│  │  └─lib
+│  ├─reorder-list
+│  ├─ssr
+│  ├─svg
+│  └─uptime
+├─dist
+├─examples
+│  ├─commits
+│  ├─elastic-header
+│  ├─firebase
+│  ├─grid
+│  ├─markdown
+│  ├─modal
+│  ├─move-animations        
+│  ├─select2
+│  ├─svg
+│  ├─todomvc
+│  └─tree
+├─flow
+├─packages
+│  ├─vue-server-renderer    
+│  │  └─types
+│  ├─vue-template-compiler  
+│  │  └─types
+│  ├─weex-template-compiler 
+│  └─weex-vue-framework     
+├─scripts
+│  └─git-hooks
+├─src
+│  ├─compiler
+│  │  ├─codegen
+│  │  ├─directives
+│  │  └─parser
+│  ├─core
+│  │  ├─components
+│  │  ├─global-api
+│  │  ├─instance
+│  │  │  └─render-helpers
+│  │  ├─observer
+│  │  ├─util
+│  │  └─vdom
+│  │      ├─helpers
+│  │      └─modules
+│  ├─platforms
+│  │  ├─web
+│  │  │  ├─compiler
+│  │  │  │  ├─directives
+│  │  │  │  └─modules
+│  │  │  ├─runtime
+│  │  │  │  ├─components
+│  │  │  │  ├─directives
+│  │  │  │  └─modules
+│  │  │  ├─server
+│  │  │  │  ├─directives
+│  │  │  │  └─modules
+│  │  │  └─util
+│  │  └─weex
+│  │      ├─compiler
+│  │      │  ├─directives
+│  │      │  └─modules
+│  │      │      └─recycle-list
+│  │      ├─runtime
+│  │      │  ├─components
+│  │      │  ├─directives
+│  │      │  ├─modules
+│  │      │  └─recycle-list
+│  │      └─util
+│  ├─server
+│  │  ├─bundle-renderer
+│  │  ├─optimizing-compiler
+│  │  ├─template-renderer
+│  │  └─webpack-plugin
+│  ├─sfc
+│  └─shared
+├─test
+│  ├─e2e
+│  │  └─specs
+│  ├─helpers
+│  ├─ssr
+│  │  └─fixtures
+│  ├─unit
+│  │  ├─features
+│  │  │  ├─component
+│  │  │  ├─directives
+│  │  │  ├─filter
+│  │  │  ├─global-api
+│  │  │  ├─instance
+│  │  │  ├─options
+│  │  │  └─transition
+│  │  └─modules
+│  │      ├─compiler
+│  │      ├─observer
+│  │      ├─server-compiler
+│  │      ├─sfc
+│  │      ├─util
+│  │      └─vdom
+│  │          ├─modules
+│  │          └─patch
+│  └─weex
+│      ├─cases
+│      │  ├─event
+│      │  ├─recycle-list
+│      │  │  └─components
+│  │      ├─runtime
+│  │      │  ├─components
+│  │      │  ├─directives
+│  │      │  ├─modules
+│  │      │  └─recycle-list
+│  │      └─util
+│  ├─server
+│  │  ├─bundle-renderer
+│  │  ├─optimizing-compiler
+│  │  ├─template-renderer
+│  │  └─webpack-plugin
+│  ├─sfc
+│  └─shared
+├─test
+│  ├─e2e
+│  │  └─specs
+│  ├─helpers
+│  ├─ssr
+│  │  └─fixtures
+│  ├─unit
+│  │  ├─features
+│  │  │  ├─component
+│  │  │  ├─directives
+│  │  │  ├─filter
+│  │  │  ├─global-api
+│  │  │  ├─instance
+│  │  │  ├─options
+│  │  │  └─transition
+│  │  └─modules
+│  │      ├─compiler
+│  │      ├─observer
+│  │      ├─server-compiler
+│  │      ├─sfc
+│  │      ├─util
+│  │      └─vdom
+│  │          ├─modules
+│  │          └─patch
+│  └─weex
+│      ├─cases
+│      │  ├─event
+│      │  ├─recycle-list
+│      │  │  └─components
+│      │  └─render
+│      ├─compiler
+│      ├─helpers
+│      └─runtime
+│          └─components
+└─types
+    └─test
---
+++ src/core/observer/index.js
@@ -0,0 +1,310 @@
+/* @flow */
+
+import Dep from './dep'
+import VNode from '../vdom/vnode'
+import { arrayMethods } from './array'
+import {
+  def,
+  warn,
+  hasOwn,
+  hasProto,
+  isObject,
+  isPlainObject,
+  isPrimitive,
+  isUndef,
+  isValidArrayIndex,
+  isServerRendering
+} from '../util/index'
+
+// getOwnPropertyNames 返回一个数组，该数组对元素是 obj自身拥有的枚举或不可枚举属性名称字符串。
+const arrayKeys = Object.getOwnPropertyNames(arrayMethods)
+
+/**
+ * In some cases we may want to disable observation inside a component's
+ * update computation.
+ */
+// 控制是否监听数据变化
+export let shouldObserve: boolean = true
+
+export function toggleObserving(value: boolean) {
+  shouldObserve = value
+}
+
+/**
+ * Observer class that is attached to each observed
+ * object. Once attached, the observer converts the target
+ * object's property keys into getter/setters that
+ * collect dependencies and dispatch updates.
+ */
+export class Observer {
+  value: any;
+  dep: Dep;
+  vmCount: number; // number of vms that have this object as root $data
+
+  constructor(value: any) {
+    this.value = value;
+    this.dep = new Dep();
+    this.vmCount = 0;
+    // 给value新增一个__ob__属性，值为该value的Observer实例
+    // 相当于为value打上标记，表示它已经被转化成响应式了，避免重复操作
+    def(value, '__ob__', this);
+    // 查看数据是否为数组
+    if (Array.isArray(value)) {
+      // 查看当前环境是否支持__proto__ 属性，这个属性会指向该对象的原型
+      if (hasProto) {
+        protoAugment(value, arrayMethods)
+      } else {
+        copyAugment(value, arrayMethods, arrayKeys)
+      }
+      this.observeArray(value)
+    } else {
+      this.walk(value)
+    }
+  }
+
+  /*
+   遍历每一个对象并且在它们上面绑定getter与setter。
+   这个方法只有在value的类型是对象的时候才能被调用
+  */
+  walk(obj: Object) {
+    // 返回回一个由一个给定对象的自身可枚举属性组成的数组
+    const keys = Object.keys(obj);
+
+    for (let i = 0; i < keys.length; i++) {
+      defineReactive(obj, keys[i])
+    }
+  }
+
+  /**
+   * Observe a list of Array items.
+   */
+  observeArray(items: Array<any>) {
+    for (let i = 0, l = items.length; i < l; i++) {
+      observe(items[i])
+    }
+  }
+}
+
+// helpers
+
+/**
+ * Augment a target Object or Array by intercepting
+ * the prototype chain using __proto__
+ */
+function protoAugment(target, src: Object) {
+  /* eslint-disable no-proto */
+  target.__proto__ = src
+  /* eslint-enable no-proto */
+}
+
+/**
+ * Augment a target Object or Array by defining
+ * hidden properties.
+ */
+/* istanbul ignore next */
+function copyAugment(target: Object, src: Object, keys: Array<string>) {
+  for (let i = 0, l = keys.length; i < l; i++) {
+    const key = keys[i]
+    def(target, key, src[key])
+  }
+}
+
+/**
+ * @description 尝试为一个值创建一个观察者实例
+ * Attempt to create an observer instance for a value,
+ * returns the new observer if successfully observed,
+ * or the existing observer if the value already has one.
+ */
+export function observe(value: any, asRootData: ?boolean): Observer | void {
+  if (!isObject(value) || value instanceof VNode) {
+    return
+  }
+
+  let ob: Observer | void;
+
+  /*
+    这里用__ob__这个属性来判断是否已经有Observer实例，
+    如果没有Observer实例则会新建一个Observer实例并赋值给__ob__这个属性，
+    如果已有Observer实例则直接返回该Observer实例
+  */
+  if (hasOwn(value, '__ob__') && value.__ob__ instanceof Observer) {
+    ob = value.__ob__
+  } else if (
+    /*
+      这里的判断是为了确保value是单纯的对象，而不是函数或者是Regexp等情况。
+      是否为服务端渲染
+      而且该对象在shouldConvert的时候才会进行Observer。这是一个标识位，避免重复对value进行Observer
+    */
+    shouldObserve &&
+    !isServerRendering() &&
+    (Array.isArray(value) || isPlainObject(value)) &&
+    Object.isExtensible(value) &&
+    !value._isVue
+  ) {
+    ob = new Observer(value)
+  }
+
+  if (asRootData && ob) {
+    ob.vmCount++
+  }
+
+  return ob
+}
+
+/**
+ * Define a reactive property on an Object.
+ * @description  使一个对象转化成可观测对象
+ */
+export function defineReactive(
+  obj: Object,
+  key: string,
+  val: any,
+  customSetter?: ?Function,
+  shallow?: boolean
+) {
+  /*在闭包中定义一个dep对象*/
+  const dep = new Dep()
+  // 返回指定对象上一个自有属性对应的属性描述符。
+  const property = Object.getOwnPropertyDescriptor(obj, key)
+
+  if (property && property.configurable === false) {
+    return
+  }
+
+  // cater for pre-defined getter/setters
+  /*
+    如果之前该对象已经预设了getter以及setter函数则将其取出来，
+    新定义的getter/setter中会将其执行，保证不会覆盖之前已经定义的getter/setter。
+  */
+  const getter = property && property.get
+  const setter = property && property.set
+  // 如果只传了obj和key，那么val = obj[key]
+  if ((!getter || setter) && arguments.length === 2) {
+    val = obj[key]
+  }
+
+  /*对象的子对象递归进行observe并返回子节点的Observer对象*/
+  let childOb = !shallow && observe(val);
+  // 给当前属性添加监听
+  Object.defineProperty(obj, key, {
+    enumerable: true,
+    configurable: true,
+    get: function reactiveGetter() {
+      const value = getter ? getter.call(obj) : val;
+
+      if (Dep.target) {
+        dep.depend();
+
+        if (childOb) {
+          childOb.dep.depend()
+          if (Array.isArray(value)) {
+            dependArray(value)
+          }
+        }
+      }
+      return value
+    },
+    set: function reactiveSetter(newVal) {
+      const value = getter ? getter.call(obj) : val
+      /* eslint-disable no-self-compare */
+      if (newVal === value || (newVal !== newVal && value !== value)) {
+        return
+      }
+      /* eslint-enable no-self-compare */
+      if (process.env.NODE_ENV !== 'production' && customSetter) {
+        customSetter()
+      }
+      // #7981: for accessor properties without setter
+      if (getter && !setter) return
+      if (setter) {
+        setter.call(obj, newVal)
+      } else {
+        val = newVal
+      }
+      childOb = !shallow && observe(newVal)
+      dep.notify()
+    }
+  })
+}
+
+/**
+ * Set a property on an object. Adds the new property and
+ * triggers change notification if the property doesn't
+ * already exist.
+ */
+export function set(target: Array<any> | Object, key: any, val: any): any {
+  if (process.env.NODE_ENV !== 'production' &&
+    (isUndef(target) || isPrimitive(target))
+  ) {
+    warn(`Cannot set reactive property on undefined, null, or primitive value: ${(target: any)}`)
+  }
+  if (Array.isArray(target) && isValidArrayIndex(key)) {
+    target.length = Math.max(target.length, key)
+    target.splice(key, 1, val)
+    return val
+  }
+  if (key in target && !(key in Object.prototype)) {
+    target[key] = val
+    return val
+  }
+  const ob = (target: any).__ob__
+  if (target._isVue || (ob && ob.vmCount)) {
+    process.env.NODE_ENV !== 'production' && warn(
+      'Avoid adding reactive properties to a Vue instance or its root $data ' +
+      'at runtime - declare it upfront in the data option.'
+    )
+    return val
+  }
+  if (!ob) {
+    target[key] = val
+    return val
+  }
+  defineReactive(ob.value, key, val)
+  ob.dep.notify()
+  return val
+}
+
+/**
+ * Delete a property and trigger change if necessary.
+ */
+export function del(target: Array<any> | Object, key: any) {
+  if (process.env.NODE_ENV !== 'production' &&
+    (isUndef(target) || isPrimitive(target))
+  ) {
+    warn(`Cannot delete reactive property on undefined, null, or primitive value: ${(target: any)}`)
+  }
+  if (Array.isArray(target) && isValidArrayIndex(key)) {
+    target.splice(key, 1)
+    return
+  }
+  const ob = (target: any).__ob__
+  if (target._isVue || (ob && ob.vmCount)) {
+    process.env.NODE_ENV !== 'production' && warn(
+      'Avoid deleting properties on a Vue instance or its root $data ' +
+      '- just set it to null.'
+    )
+    return
+  }
+  if (!hasOwn(target, key)) {
+    return
+  }
+  delete target[key]
+  if (!ob) {
+    return
+  }
+  ob.dep.notify()
+}
+
+/**
+ * Collect dependencies on array elements when the array is touched, since
+ * we cannot intercept array element access like property getters.
+ */
+function dependArray(value: Array<any>) {
+  for (let e, i = 0, l = value.length; i < l; i++) {
+    e = value[i]
+    e && e.__ob__ && e.__ob__.dep.depend()
+    if (Array.isArray(e)) {
+      dependArray(e)
+    }
+  }
+}
